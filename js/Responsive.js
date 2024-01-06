b2a7b11293312407f6905bcdb8e19625cd127f6f
Chilengue <chilenguejoseorlando@gmail.com>
2024-01-07 01:58:18 +0200

    first commit

---
+++ js/Responsive.js
@@ -0,0 +1,10 @@
+const toglleBtn=document.querySelector('.toglle_btn')
+const toglleBtnIcon=document.querySelector('.toggle_btn i')
+const dropDownMenu =document.querySelector('dropdown_menu')
+
+toglleBtn.onclick=function (){
+    dropDownMenu.classList.toggle('open')
+    const isOpen =dropDownMenu.classList.contains('open')
+
+    toggle
+}
